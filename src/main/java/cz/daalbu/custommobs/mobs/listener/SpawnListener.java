d5e3d7b7cf9dd3c2cabbca72a9b16ce08df4c2ac
BuildTools <unconfigured@null.spigotmc.org>
2023-05-30 16:41:37 +0200

    Add SpawnListener class

---
+++ src/main/java/cz/daalbu/custommobs/mobs/listener/SpawnListener.java
@@ -0,0 +1,18 @@
+package cz.daalbu.custommobs.mobs.listener;
+
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntitySpawnEvent;
+
+public class SpawnListener implements Listener {
+
+    @EventHandler
+    public void onSpawn(EntitySpawnEvent event) {
+        EntityType entity = event.getEntityType();
+        v
+
+
+    }
+
+}
