0b619f1f846f775bb77238f5857392b5ec5aac3a
BuildTools <unconfigured@null.spigotmc.org>
2023-05-30 16:41:50 +0200

    Add MobController class

---
+++ src/main/java/cz/daalbu/custommobs/mobs/controller/MobController.java
@@ -0,0 +1,11 @@
+package cz.daalbu.custommobs.mobs.controller;
+
+import cz.daalbu.custommobs.CustomMobs;
+
+public class MobController {
+    private CustomMobs plugin;
+
+    public MobController() {
+        this.plugin = CustomMobs.getInstance();
+    }
+}
